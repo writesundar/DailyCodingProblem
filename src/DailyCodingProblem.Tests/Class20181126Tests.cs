e427de29c79aa8bf3fffeb3f0f0722d5d6b99745
som <somanna.ganesh@gmail.com>
2018-11-27 21:57:40 +0000

    Solutions for daily code problems 2018-11-26,27

---
+++ src/DailyCodingProblem.Tests/Class20181126Tests.cs
@@ -0,0 +1,24 @@
+﻿using DailyCodingProblem.Problem20181126;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests
+{
+    [TestFixture]
+    public class Class20181126Tests
+    {
+        [TestCase(new int[] {10, 15, 3, 7}, 17, true)]
+        [TestCase(new int[] { 10, 15, 3, 7 }, 18, true)]
+        [TestCase(new int[] { 10, 15, 3, 7 }, 1, false)]
+        [TestCase(new int[] { 10, -15, 3 }, -5, true)]
+        public void DoesTwoNumbersAddUpTests(int[] array, int number, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Class20181126.DoesTwoNumbersAddUp(array, number);
+
+            // Assert
+            Assert.That(actual, Is.True);
+        }
+    }
+}
---
+++ src/DailyCodingProblem/Problem20181127/Class20181127.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DailyCodingProblem.Problem20181127
+{
+    public class Class20181127
+    {
+        public static int[] TransformUsingDivision(int[] input)
+        {
+            var transformed = new int[input.Length];
+            long allMultiplied = 1;
+            foreach (var i in input)
+            {
+                allMultiplied *= i;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                transformed[i] = (int)(allMultiplied / input[i]);
+            }
+
+            return transformed;
+        }
+    }
+}
